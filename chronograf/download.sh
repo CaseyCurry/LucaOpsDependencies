962249f363fd41dcab800f18e753c824e922f36b
CaseyCurry <casey.de.curry@gmail.com>
2017-09-12 16:20:16 -0500

    replace BASE_PATH - it doesn't work - in favor of --basepath

--- chronograf/download.sh
+++ chronograf/download.sh
@@ -10,8 +10,8 @@ acbuild begin
 acbuild set-name chronograf
 acbuild label add chronograf 1.3.8.1
 acbuild copy ./chronograf-1.3.8.1-1/chronograf /chronograf
-acbuild set-exec /chronograf
-acbuild port add http tcp 8888
+acbuild set-exec -- /chronograf --basepath=/chronograf
+acbuild port add db tcp 8888
 acbuild write ./chronograf.1.3.8.1.aci
 acbuild end
 
