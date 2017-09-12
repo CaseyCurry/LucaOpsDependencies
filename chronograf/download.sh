de4c4cfc206b51930294388612946582e34a1f90
CaseyCurry <casey.de.curry@gmail.com>
2017-09-12 16:44:17 -0500

    fix set-exec; change db port name to http

--- chronograf/download.sh
+++ chronograf/download.sh
@@ -11,7 +11,7 @@ acbuild set-name chronograf
 acbuild label add chronograf 1.3.8.1
 acbuild copy ./chronograf-1.3.8.1-1/chronograf /chronograf
 acbuild set-exec -- /chronograf --basepath=/chronograf
-acbuild port add db tcp 8888
+acbuild port add http tcp 8888
 acbuild write ./chronograf.1.3.8.1.aci
 acbuild end
 
